e5abee9e1ff1042ac6317c022e39d750a5d66d79
niveditapurohitt <nivirajpurohit5678@gmail.com>
2026-07-02 04:05:10 +0530

    api

--- admin-app/SKRT-Transport-SKRT_V2/frontend/src/lib/api.ts
+++ admin-app/SKRT-Transport-SKRT_V2/frontend/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
-
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://skrt-website.onrender.com/api";
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
